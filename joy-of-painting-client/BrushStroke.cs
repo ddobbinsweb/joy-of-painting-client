b9b7e78b24b8c9e281f5b4eafb21730ebd323dc1
David Dobbins <david.dobbins@revunit.com>
2023-07-07 15:01:42 -0400

    dotnet console app

---
+++ joy-of-painting-client/BrushStroke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace joy_of_painting_client
+{
+    public class BrushStroke
+    {
+        public int FromX { get; set; }
+        public int FromY { get; set; }
+        public int ToX { get; set; }
+        public int ToY { get; set; }
+        public string Color { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
---
+++ joy-of-painting-client/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using joy_of_painting_client;
+using Newtonsoft.Json;
+using System.Drawing;
+
+
+Console.WriteLine("Hello, World!");
+// Specifying a file path
+string path = @"C:\Users\David Dobbins\Pictures\joy-of-painting\8_Dobbins.jpg";
+
+
+Bitmap bitmap;
+using (Stream bmpStream = System.IO.File.Open(path, System.IO.FileMode.Open))
+{
+    Image image = Image.FromStream(bmpStream);
+
+    bitmap = new Bitmap(image);
+
+}
+if (bitmap != null)
+{
+    List<BrushStroke> strokes = new List<BrushStroke>();
+    for (int i = 0; i < bitmap.Width; i++)
+    {
+        for (int j = 0; j < bitmap.Height; j++)
+        {
+            Color pixel = bitmap.GetPixel(i, j);
+            BrushStroke stroke = new BrushStroke() { 
+                FromX= ,
+                FromY= ,
+                ToX =,
+                ToY = ,
+                Color = ColorTranslator.ToHtml(pixel),
+                Height= j,
+                Width= i
+            };
+
+            strokes.Add(stroke);
+        }
+    }
+    if(strokes.Count > 0)
+    {
+        var temp = JsonConvert.SerializeObject(strokes);
+        var temp2  = 0;
+    }
+}
+

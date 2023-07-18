65b399ca7193233e9e8ee2ae765beff50ab78d01
David Dobbins <david.dobbins@maersk.com>
2023-07-18 11:22:14 -0400

    add helper for downloading images

---
+++ joy-of-painting-client/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace joy_of_painting_client
+{
+    public static class Helper
+    {
+
+        public static async Task DownloadImageAsync(string directoryPath, string fileName, Uri uri)
+        {
+            using var httpClient = new HttpClient();
+
+            // Get the file extension
+            var uriWithoutQuery = uri.GetLeftPart(UriPartial.Path);
+            var fileExtension = Path.GetExtension(uriWithoutQuery);
+
+            // Create file path and ensure directory exists
+            var path = Path.Combine(directoryPath, $"{fileName}{fileExtension}");
+            Directory.CreateDirectory(directoryPath);
+
+            // Download the image and write to the file
+            var imageBytes = await httpClient.GetByteArrayAsync(uri);
+            await File.WriteAllBytesAsync(path, imageBytes);
+        }
+    }
+}

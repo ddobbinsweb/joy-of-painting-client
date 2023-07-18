692666777f75d68f7e1206a35ef1d13514b5e43a
David Dobbins <david.dobbins@maersk.com>
2023-07-18 18:15:47 -0400

    save preview file before uploading

--- joy-of-painting-client/Pixelator.cs
+++ joy-of-painting-client/Pixelator.cs
@@ -1,13 +1,20 @@
-﻿using System.Drawing;
+﻿using joy_of_painting_client.Models;
+using Spectre.Console;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.CompilerServices;
+using Color = System.Drawing.Color;
 
 namespace joy_of_painting_client;
 
 public class Pixelator
 {
-    public List<Brushstroke> PixelateImage(string imagePath, int size = 20)
+    private const string FILE_PATH = $@"C:\Users\David Dobbins\Pictures\joy-of-painting\";
+    public async Task<List<Brushstroke>> PixelateImageAsync(Painting painting, int size = 20, int failureCount = 0)
     {
         List<Brushstroke> brushstrokes = new();
-
+        await Helper.DownloadImageAsync(FILE_PATH, painting.Id.ToString(), new Uri(painting.Url));
+        string imagePath = $"{FILE_PATH}/{painting.Id}.jpg";
         Bitmap image = new Bitmap(imagePath);
 
         int width = image.Width;
@@ -43,7 +50,72 @@ public class Pixelator
                 order++;
             }
         }
+        if (brushstrokes.Count > 1000)
+        {
+           await PixelateImageAsync(painting, size, failureCount + 1);
+        }
+
+
+        RotateBrushstrokes(brushstrokes, width, height);
+        string previewImagePath = $@"{FILE_PATH}\preview\{DateTime.Now.ToFileTimeUtc}";
+
+        CreateImageStream(width, height, brushstrokes, previewImagePath);
+        var previewImage = new CanvasImage(previewImagePath);
+        previewImage.MaxWidth = pixelSize;
+
+        AnsiConsole.Write(previewImage);
+
+        if (!AnsiConsole.Confirm("Does this look good?"))
+        {
+           await  PixelateImageAsync(painting, size);
+        }
         return brushstrokes;
+    }
+
+    public  void CreateImageStream(int width, int height, List<Brushstroke> brushstrokes, string savePath)
+    {
+        using (Bitmap bitmap = new Bitmap(width, height))
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                ColorConverter colorConverter = new ColorConverter();
+                g.Clear(System.Drawing.Color.White);
+
+                foreach (Brushstroke stroke in brushstrokes.OrderBy(x => x.Order).ToList())
+                {
+                    Pen pen = new Pen((System.Drawing.Color)colorConverter.ConvertFromString(stroke.Color));
+                    pen.Width = stroke.Width;
+                    g.DrawLine(pen, stroke.FromX, stroke.FromY, stroke.ToX, stroke.ToY);
+                }
+            }
+            bitmap.Save(savePath, ImageFormat.Jpeg);
+        }
+    }
+    private void RotateBrushstrokes(List<Brushstroke> brushstrokes, int width, int height)
+    {
+        foreach (var brushstroke in brushstrokes)
+        {
+            int centerX = brushstroke.FromX + (brushstroke.Width / 2);
+            int centerY = brushstroke.FromY + (brushstroke.Height / 2);
+
+            double angle = Math.PI / 4; // 45 degrees clockwise rotation
+
+            int rotatedFromX = (int)Math.Round((brushstroke.FromX - centerX) * Math.Cos(angle) - (brushstroke.FromY - centerY) * Math.Sin(angle) + centerX);
+            int rotatedFromY = (int)Math.Round((brushstroke.FromX - centerX) * Math.Sin(angle) + (brushstroke.FromY - centerY) * Math.Cos(angle) + centerY);
+            int rotatedToX = (int)Math.Round((brushstroke.ToX - centerX) * Math.Cos(angle) - (brushstroke.ToY - centerY) * Math.Sin(angle) + centerX);
+            int rotatedToY = (int)Math.Round((brushstroke.ToX - centerX) * Math.Sin(angle) + (brushstroke.ToY - centerY) * Math.Cos(angle) + centerY);
+
+            brushstroke.FromX = rotatedFromX;
+            brushstroke.FromY = rotatedFromY;
+            brushstroke.ToX = rotatedToX;
+            brushstroke.ToY = rotatedToY;
+
+            // Ensure the coordinates are within the image bounds
+            brushstroke.FromX = Math.Max(0, Math.Min(brushstroke.FromX, width - 1));
+            brushstroke.FromY = Math.Max(0, Math.Min(brushstroke.FromY, height - 1));
+            brushstroke.ToX = Math.Max(0, Math.Min(brushstroke.ToX, width - 1));
+            brushstroke.ToY = Math.Max(0, Math.Min(brushstroke.ToY, height - 1));
+        }
     }
 
     private Color CalculateAverageColor(Bitmap image, int fromX, int fromY, int toX, int toY)

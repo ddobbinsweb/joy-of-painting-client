65fccff53d353f540a670473f3b39ba08ad7acd5
David Dobbins <david.dobbins@maersk.com>
2023-07-18 11:22:57 -0400

    add pixelation model

---
+++ joy-of-painting-client/Models/Pixelation.cs
@@ -0,0 +1,10 @@
+﻿namespace joy_of_painting_client.Models;
+public class Pixelation
+{
+    public Pixelation()
+    {
+    }
+
+    public List<Brushstroke> Brushstrokes { get; set; }
+    public int PaintingId { get; set; }
+}

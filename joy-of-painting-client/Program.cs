e25a6b0c97fcbd2e2af0d0bb5ae7cd36e9089f3f
David Dobbins <david.dobbins@maersk.com>
2023-07-19 18:23:16 -0400

    check config values, setup game menu nav

--- joy-of-painting-client/Program.cs
+++ joy-of-painting-client/Program.cs
@@ -2,29 +2,83 @@
 using joy_of_painting_client.Models;
 using joy_of_painting_client.Responses;
 using Spectre.Console;
-using System.Drawing;
+using System.Data;
+
+
+var panel = new Panel(
+          Align.Center(
+              new Markup("Welcome to Joy of Painting!"),
+              VerticalAlignment.Middle));
+AnsiConsole.Write(panel);
+// get config settings
+GameSettings.Check();
+string? userKey = GameSettings.GetUserKey();
+
 
 do
 {
-    AnsiConsole.WriteLine("Joy of Painting!");
+  
+
+    /*
+    *TODO: change app commands
+    * first options-
+    * pixelate image
+    *  - sub options
+    *      find paintings by Artist or Category
+    *      
+    * view leaderboard
+    * create orignal
+    *
+    */
+
+    /*
+     *  TODO: ADD game navigation
+     *  
+     */
+
+    string gameMenu = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+            .Title("[bold white]--Game Menu-- [/]")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
+            .AddChoices("Pixelate Image", "View LeaderBoard", "Settings", "Exit")
+            );
+    switch (gameMenu)
+    {
+        case "Pixelate Image":
+            //Make code go to pixalor
+            AnsiConsole.WriteLine("TODO:");
+            break;
+        case "View LeaderBoard":
+            AnsiConsole.WriteLine("TODO:");
+            break;
+        case "Settings":
+            AnsiConsole.WriteLine("TODO:");
+            break;
+            break;
+        case "Exit":
+            Environment.Exit(0);
+            break;
+        default:
+            break;
+    }
 
     // TODO: make this an input
-    var pixelator = new Pixelator();
-    string key = AnsiConsole.Prompt(new TextPrompt<string>("[grey][[Optional]][/] [green] What's your api key[/]?")
-                                         .DefaultValue<string>("486c153f-e4f0-4657-8ac8-fe3850bb51ad"));
+    Pixelator pixelator = new();
 
-    var findPaintingOption = AnsiConsole.Prompt(
+
+    string findPaintingOption = AnsiConsole.Prompt(
         new SelectionPrompt<string>()
-            .Title("[green] How would you like to find a painting  by [/]?")
+            .Title("[green] How would you like to find a painting by [/]?")
             .PageSize(10)
             .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
             .AddChoices("Artist", "Category")
             );
-
+    CommandProcesor.ProcessCommand(findPaintingOption);
 
     if (findPaintingOption == "Artist")
     {
-        var artistClient = new BaseClient<ListResponse<Artist>>("artist/search", key);
+        var artistClient = new BaseClient<ListResponse<Artist>>("artist/search", userKey);
         // get artists
         var values = new Dictionary<string, string?>
         {
@@ -52,6 +106,7 @@ do
             if (selectedArtist != null)
             {
                 AnsiConsole.MarkupLine("You selected: [yellow]{0}[/]", selectedArtist);
+
                 var artistPaintings = artists.FirstOrDefault(x => x.Name == selectedArtist).Paintings.Select(x => x.Name);
 
                 var selectedPainting = AnsiConsole.Prompt(new SelectionPrompt<string>()
@@ -60,6 +115,7 @@ do
                   .AddChoices(artistPaintings)
                   );
                 AnsiConsole.MarkupLine("You selected: [yellow]{0}[/]", selectedPainting);
+
                 var painting = artists.FirstOrDefault(x => x.Name == selectedArtist)?.Paintings.Find(x => x.Name == selectedPainting);
 
                 if (painting != null)
@@ -68,10 +124,17 @@ do
                                          .DefaultValue<int>(20));
 
                     AnsiConsole.MarkupLine("Pixelating image");
-                    await Helper.DownloadImageAsync($@"C:\Users\David Dobbins\Pictures\joy-of-painting\", painting.Id.ToString(), new Uri(painting.Url));
+                    string BaseImagePath = $@"C:\Users\David Dobbins\Pictures\joy-of-painting\"; // TODO:get from configuration
+
+                    await Helper.DownloadImageAsync(BaseImagePath, painting.Id.ToString(), new Uri(painting.Url));
+
+                    string originalImagePath = $@"{BaseImagePath}{painting.Id}.jpg";
+                    CanvasImage originalImage = new(originalImagePath);
+                    originalImage.MaxWidth(pixelSize);
+
 
                     List<Brushstroke> strokes = await pixelator.PixelateImageAsync(painting, pixelSize);
-                  
+
                     Pixelation pixelation = new()
                     {
                         Brushstrokes = strokes,
@@ -83,7 +146,7 @@ do
                     await AnsiConsole.Status().StartAsync("Loading...", async ctx =>
                     {
                         // upload pixalation
-                        var pixelationClient = new BaseClient<PixelationResponse>("pixelation", key);
+                        var pixelationClient = new BaseClient<PixelationResponse>("pixelation", userKey);
                         ctx.Status("Uploading pixelation");
                         ctx.Spinner(Spinner.Known.Star);
                         ctx.SpinnerStyle(Style.Parse("green"));
@@ -99,10 +162,9 @@ do
                         }
                         await Helper.DownloadImageAsync($@"C:\Users\David Dobbins\Pictures\joy-of-painting\submissions", pixelationResponse.Id.ToString(), new Uri(pixelationResponse.Url));
 
-
-                        var image = new CanvasImage($@"C:\Users\David Dobbins\Pictures\joy-of-painting\submissions\{pixelationResponse.Id}.jpg");
+                        string imagePath = $@"C:\Users\David Dobbins\Pictures\joy-of-painting\submissions\{pixelationResponse.Id}.jpg";
+                        CanvasImage image = new (imagePath);
                         image.MaxWidth(pixelSize);
-                        AnsiConsole.Write(image);
 
                         AnsiConsole.WriteLine(pixelationResponse.Message);
                     }

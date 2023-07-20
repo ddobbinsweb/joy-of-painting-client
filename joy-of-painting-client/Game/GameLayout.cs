9f82213dd8166076fd2255f33b2fc4aa7e9cf637
David Dobbins <david.dobbins@maersk.com>
2023-07-20 18:11:23 -0400

    get painter in header

--- joy-of-painting-client/Game/GameLayout.cs
+++ joy-of-painting-client/Game/GameLayout.cs
@@ -1,39 +1,82 @@
-﻿using Spectre.Console;
+﻿using joy_of_painting_client.Models;
+using joy_of_painting_client.Responses;
+using Spectre.Console;
 using Spectre.Console.Rendering;
+using System.Reflection.Metadata.Ecma335;
 
 namespace joy_of_painting_client.Game
 {
-    public class GameLayout
+    public static class GameLayout
     {
-       // private static Layout _layout;
+        // private static Layout _layout;
         public static void Setup()
         {
             // create layout
             // Create the layout
-            // _layout = new Layout("Top");
+            //  _layout = new Layout("Top");
             // .SplitRows(new Layout("Top"));
 
 
             // Update the Top column
-            // Update("Top", Header());
+            //  Update("Top", Header());
             // Update("Center", Center());
 
             // Render the layout
             InsertHeader();
             // add Header
+            // AnsiConsole.Write(_layout);
 
         }
-        public static void InsertHeader()
+        public static async void InsertHeader()
         {
-            AnsiConsole.Write(Header());
+            AnsiConsole.Write(await HeaderAsync());
         }
-        private static Panel Header()
+        private static async Task<Panel> HeaderAsync()
         {
+            Markup userContent;
+            Panel header;
+            var userkey = GameSettings.GetUserKey();
+            if (userkey != null)
+            {
+                // TODO: move this to app startup and cache values
+                // get Player
+                var painterClient = new BaseClient("painter", userkey);
+
+                var userResponse = await painterClient.GetAllAsync<SingleResponse<Painter>>("/me");
+                if (userResponse.Item != null)
+                {
+                    // insert player 
+                    var grid = new Grid();
+
+                    // Add columns 
+                    grid.AddColumn();
+                    grid.AddColumn();
+                    grid.AddColumn();
+
+                    // Add header row 
+                    grid.AddRow("Player", "Score", "Profile");
+                    grid.AddRow(
+                        $"{userResponse.Item.Name}",
+                        $"{userResponse.Item.Pixelations.Sum(x => x.Score)}",
+                        $"[link green] https://jop.revunit.com/profile/{userResponse.Item.Id} [/]"
+                    );
+
+                    // Write to Console
+                    Panel panel = new(grid);
+                    return new Panel(grid)
+                          .Expand()
+                          .SquareBorder()
+                          .Header("[red]Welcome to Joy of Painting[/]");
+                }
+
+
+            }
             // Header
-            return new Panel(Align.Center(new Markup("Welcome to Joy of Painting!"), VerticalAlignment.Middle));
 
-           // AnsiConsole.Write(panel);
-
+            return new Panel(new Text("Joy of Painting!").Centered())
+                .Expand()
+                .SquareBorder()
+                .Header("[red]Welcome to[/]");
         }
         private static Panel Center()
         {

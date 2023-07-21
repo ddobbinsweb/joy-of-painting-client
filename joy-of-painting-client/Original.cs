6c2b7862956745210b4f71127f2c19cb9dd5c8c7
David Dobbins <david.dobbins@maersk.com>
2023-07-21 18:12:14 -0400

    pass key to frontend

--- joy-of-painting-client/Original.cs
+++ joy-of-painting-client/Original.cs
@@ -1,15 +1,20 @@
-﻿using Spectre.Console;
+﻿using joy_of_painting_client.Game;
+using Spectre.Console;
 
 namespace joy_of_painting_client
 {
     public static class Original
     {
         //TODO: get from app settings
-        private static string _originalSite = "https://joy-of-painting-client.vercel.app/";
+       // private static string _originalSite = "https://joy-of-painting-client.vercel.app/";
+        private static string _originalSite = "http://localhost:3000/";
         public static void Create()
         {
+            string? userKey = GameSettings.GetUserKey();
             AnsiConsole.MarkupLine("To Create an Original Masterpiece");
             AnsiConsole.MarkupLine($"-  Navigate to {_originalSite}");
+            Helper.OpenBrowser($"{_originalSite}?key={userKey}");
+
             AnsiConsole.Confirm("All Done?");
         }
     }

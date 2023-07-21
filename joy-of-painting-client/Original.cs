b8ba8e21bf7d29bbe7e43a258426cdeba1827175
David Dobbins <david.dobbins@maersk.com>
2023-07-21 15:01:35 -0400

    add Original Class

---
+++ joy-of-painting-client/Original.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console;
+
+namespace joy_of_painting_client
+{
+    public static class Original
+    {
+        //TODO: get from app settings
+        private static string _originalSite = "https://joy-of-painting-client.vercel.app/";
+        public static void Create()
+        {
+            AnsiConsole.MarkupLine("To Create an Original Masterpiece");
+            AnsiConsole.MarkupLine($"-  Navigate to {_originalSite}");
+            AnsiConsole.Confirm("All Done?");
+        }
+    }
+}

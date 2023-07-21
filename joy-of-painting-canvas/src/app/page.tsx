e96084891cd59c238b2907f39a5183096ed964f8
David Dobbins <david.dobbins@maersk.com>
2023-07-21 18:13:20 -0400

    get key and submit to api

--- joy-of-painting-canvas/src/app/page.tsx
+++ joy-of-painting-canvas/src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 import {
+  AppBar,
   Button,
   Card,
   CardContent,
+  Container,
   List,
   ListItem,
-  Typography,
+  Toolbar,
+  Typography
 } from "@mui/material";
+import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { ChromePicker } from "react-color";
 import { useDraw } from "./hooks/useDraw";
@@ -14,6 +18,9 @@ import { useDraw } from "./hooks/useDraw";
 export default function Home() {
   const [color, setColor] = useState("#000");
   const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
+
+  const searchParams = useSearchParams();
+  const key = searchParams.get("key");
 
   function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
     const { x: currX, y: currY } = currentPoint;
@@ -38,7 +45,7 @@ export default function Home() {
 
     const canvas = canvasRef.current;
     // Convert canvas data to data URL
-    const dataURL = canvas.toDataURL('image/png');
+    const dataURL = canvas.toDataURL("image/png");
 
     // Create an image element
     const img = new Image();
@@ -47,12 +54,12 @@ export default function Home() {
     // Wait for the image to load before continuing
     img.onload = () => {
       // Create a new canvas to draw the image
-      const newCanvas = document.createElement('canvas');
+      const newCanvas = document.createElement("canvas");
       newCanvas.width = img.width;
       newCanvas.height = img.height;
 
       // Get the context of the new canvas
-      const newCtx = newCanvas.getContext('2d');
+      const newCtx = newCanvas.getContext("2d");
       if (newCtx) {
         newCtx.drawImage(img, 0, 0);
 
@@ -61,73 +68,83 @@ export default function Home() {
           if (blob) {
             // Now you can submit the 'blob' to your API using XMLHttpRequest, fetch, or any other method you prefer
             // For example, if you're using fetch:
-            fetch('https://api.jop.revunit.com/original', {
-              method: 'POST',
+            fetch("https://api.jop.revunit.com/original", {
+              method: "POST",
               body: blob,
               headers: {
-                'Content-Type': 'image/jpeg', // adjust the content type based on the image format you are using (e.g., 'image/jpeg' for JPEG)
+                "Content-Type": "image/jpeg", // adjust the content type based on the image format you are using (e.g., 'image/jpeg' for JPEG)
               },
             })
               .then((response) => {
                 // Handle the API response here
-                console.log('API Response:', response);
+                console.log("API Response:", response);
               })
               .catch((error) => {
                 // Handle errors here
-                console.error('Error:', error);
+                console.error("Error:", error);
               });
           }
         });
       }
-    }
-  }
-  
-  
+    };
+  };
+
   return (
     <>
       {/* Nav Bar */}
-      <nav className="flex justify-left h-auto">
-        <h1 className="text-xxl text-white pl-5">Happy Little Trees</h1>
-      </nav>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar>
+              <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+                Happy Little Trees
+              </Typography>
+        
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Api key: {key}
+              </Typography>
+         
+          </Toolbar>
+        </Container>
+      </AppBar>
+
       <main>
         {/* Left Column */}
-      <div className=" bg-gradient-to-r from-cyan-500 to-blue-500 flex justify-center items-center p-5">
-        <div className="flex flex-col gap-10 pr-10">
-          <ChromePicker onChange={(e: any) => setColor(e.hex)} />
-          <Button onClick={submit} variant="contained">
-            Submit
-          </Button>
-          <Button onClick={clear} variant="contained">
-            Clear
-          </Button>
+        <div className=" bg-gradient-to-r from-cyan-500 to-blue-500 flex justify-center items-center p-5">
+          <div className="flex flex-col gap-10 pr-10">
+            <ChromePicker onChange={(e: any) => setColor(e.hex)} />
+            <Button onClick={submit} variant="contained">
+              Submit
+            </Button>
+            <Button onClick={clear} variant="contained">
+              Clear
+            </Button>
+          </div>
+          {/* center column */}
+          <div className="flex flex-col gap-10 rounded-md bg-white">
+            <canvas
+              onMouseDown={onMouseDown}
+              ref={canvasRef}
+              width={750}
+              height={750}
+              className="border border-black rounded-md"
+            />
+          </div>
+          {/* center column */}
+          <div className="flex flex-col gap-10 pl-10">
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  How to use
+                </Typography>
+                <List>
+                  <ListItem>click and drag the mouse</ListItem>
+                  <ListItem>when done click submit</ListItem>
+                </List>
+              </CardContent>
+            </Card>
+          </div>
         </div>
-        {/* center column */}
-        <div className="flex flex-col gap-10 rounded-md bg-white">
-          <canvas
-            onMouseDown={onMouseDown}
-            ref={canvasRef}
-            width={750}
-            height={750}
-            className="border border-black rounded-md"
-          />
-        </div>
-        {/* center column */}
-        <div className="flex flex-col gap-10 pl-10">
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography variant="h5" component="div">
-                How to use
-              </Typography>
-              <List>
-                <ListItem>click and drag the mouse</ListItem>
-                <ListItem>when done click submit</ListItem>
-              </List>
-            </CardContent>
-          </Card>
-        </div>
-      </div>
       </main>
-      
     </>
   );
 }
